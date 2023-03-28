7aef365d67534259e59efdeddb14cd479c700999
windstarry <liefengwind@163.com>
2023-03-28 12:27:58 +0800

    增加编辑配置

--- docs/.vuepress/config/themeVdoingConfig.ts
+++ docs/.vuepress/config/themeVdoingConfig.ts
@@ -10,6 +10,7 @@ export default <VdoingThemeConfig>{
   // repo: "Windstarry/windstarry.github.io", // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: "上次更新", // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+  docsRepo: 'Windstarry/windstarry.github.io',
   docsDir: "docs", // 编辑的文件夹
   editLinks: true, // 启用编辑
   editLinkText: "编辑", // 编辑链接文字
